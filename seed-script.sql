f3fc343b30f48c785caa212e093dc1647009bacc
Pierre Laffaille <pierre.laffaille@outlook.fr>
2025-02-07 16:14:48 +0100

    added base scripts

---
+++ seed-script.sql
@@ -0,0 +1,5 @@
+USE spinit;
+
+INSERT INTO Users (id, pfp, name, bio, email, pwd) VALUES (2, "#", "John2", "bio", "john2@john.fr", 'john');
+
+INSERT INTO Releases (id, cover, title, discogs_release_id, artists, release_date) value (12, "#", "Funeralopolis", 123456, "Electric Wizard", "2011-01-01");
